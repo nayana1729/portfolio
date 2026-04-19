2496751b85f9fcd4fe514a30cabbdf1941545d02
nayana1729 <nayana.naineni111@gmail.com>
2026-04-18 19:06:08 -0700

    lab 3

---
+++ global.js
@@ -0,0 +1,67 @@
+console.log("IT’S ALIVE!");
+
+function $$(selector, context = document) {
+  return Array.from(context.querySelectorAll(selector));
+}
+
+let pages = [
+  { url: 'index.html', title: 'Home' },
+  { url: 'resume.html', title: 'Resume' },
+  { url: 'contact/index.html', title: 'Contact' },
+  { url: 'projects/index.html', title: 'Projects' },
+  { url: 'https://github.com/nayana1729', title: 'Github' },
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"                  // Local server
+  : "/website/";         // GitHub Pages repo name
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+  url = !url.startsWith('http') ? BASE_PATH + url : url;
+  let a = document.createElement('a');
+  a.href = url;
+  a.textContent = title;
+  nav.append(a);
+
+  if (a.host === location.host && a.pathname === location.pathname) {
+    a.classList.add('current');
+  }
+
+  if (a.host !== location.host) {
+    a.target = "_blank";
+  }
+}
+
+document.body.insertAdjacentHTML(
+  'afterbegin',
+  `
+  <label class="color-scheme">
+    Theme:
+    <select>
+      <option value="light dark">Automatic</option>
+      <option value="light">Light</option>
+      <option value="dark">Dark</option>
+    </select>
+  </label>`,
+);
+
+let select = document.querySelector('.color-scheme select');
+
+function setColorScheme(colorScheme) {
+  document.documentElement.style.setProperty('color-scheme', colorScheme);
+  select.value = colorScheme;
+}
+
+if ('colorScheme' in localStorage) {
+  setColorScheme(localStorage.colorScheme);
+}
+
+select.addEventListener('input', function (event) {
+  localStorage.colorScheme = event.target.value;
+  setColorScheme(event.target.value);
+});
